e0284693ca358dca3dead22c13301bdabc09f684
franzonistefano <franzoni.stefano93@gmail.com>
2020-05-13 17:25:55 +0200

    Fix config file (typo) Adapt fab script

--- fab_script/fab1.sh
+++ fab_script/fab1.sh
@@ -7,7 +7,7 @@ export GOPATH=$HOME/go
 export FAB3_CONFIG=${GOPATH}/src/github.com/hyperledger/fabric-chaincode-evm/examples/network-sdk-config.yaml # Path to a compatible Fabric SDK Go config file
 export FAB3_USER=User1 # User identity being used for the proxy (Matches the users names in the crypto-config directory specified in the config)
 export FAB3_ORG=Org1  # Organization of the specified user
-export FAB3_CHANNEL=channelall # Channel to be used for the transactions
+export FAB3_CHANNEL=channel12 # Channel to be used for the transactions
 export FAB3_CCID=evmcc # ID of the EVM Chaincode deployed in your fabric network
 export FAB3_PORT=5000 # Port the proxy will listen on. If not provided default is 5000.
 
